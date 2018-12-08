874faf603a0f855f764098d9093300384c2b06dd
Marcelo Rodriguez <cybermarce12@gmail.com>
2018-12-08 14:04:38 -0300

    Agregado de Login

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
@@ -15,6 +16,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [
       AppComponent,
       HomeComponent,
+      LoginComponent,
   ],
   imports: [
       NativeScriptModule,
